7ec9af8230e52a814d82ed87766dac58845e7921
kombou <yvan.kombou@gmail.com>
2018-10-26 12:52:47 +0100

    add periode

--- ClientApp/src/app/time-table/classe/classe.component.ts
+++ ClientApp/src/app/time-table/classe/classe.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {Classe} from '../../models/Classe';
+import {Periode} from '../../models/Periode';
 import {Class} from '../classe';
 import {Time} from '../../models/Time';
 import {TimeTableRepository} from '../time-table.repository';
@@ -11,40 +12,21 @@ import {TimeTableRepository} from '../time-table.repository';
 })
 export class ClasseComponent {
   current: Classe;
+  current2:Periode;
   timeTable = [];
-  timeLundi = [];
-  timeMardi = [];
-  timeMercredi = [];
-  timeJeudi = [];
-  timeVendredi = [];
-  timeSamedi = [];
+  periodeTable= [];
 
   constructor(private classe: Class, private repository: TimeTableRepository) {
     this.classe.classe.subscribe(c => {
-      console.log(this.current);
+      console.log(this.current + "ffffff");
     });
+   
     this.repository.list(this.current).subscribe(l => {
-      this.timeTable = l
-      const group = this.groupBy(l, pet => pet.idjourNavigation.nom);
-      this.timeLundi = group.get('LUNDI');
-      this.timeMardi = group.get('MARDI');
-      this.timeMercredi = group.get('MERCREDI');
-      this.timeJeudi = group.get('JEUDI');
-      this.timeVendredi = group.get('VENDREDI');
-      this.timeSamedi = group.get('SAMEDI');
+      this.timeTable = l;
     });
-  }
-groupBy(list, keyGetter) {
-    const map = new Map();
-    list.forEach((item) => {
-      const key = keyGetter(item);
-      const collection = map.get(key);
-      if (!collection) {
-        map.set(key, [item]);
-      } else {
-        collection.push(item);
-      }
+
+    this.repository.listPeriode(this.current2).subscribe(p => {
+      this.periodeTable = p;
     });
-    return map;
   }
 }
