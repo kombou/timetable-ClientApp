21c0ef143f77bd24e1a6aecdee95fe4ec27d089e
FranckTONYA <francktonya7@gmail.com>
2018-10-26 07:21:02 +0200

    MAJ recuperation TimeTable

--- ClientApp/src/app/time-table/time-table.repository.ts
+++ ClientApp/src/app/time-table/time-table.repository.ts
@@ -2,14 +2,20 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Classe} from '../models/Classe';
 import {Time} from '../models/Time';
+import { Periode } from '../models/Periode';
 
 @Injectable()
 export class TimeTableRepository {
   url = 'http://localhost:5000/api/times';
+  urlPeriode = 'http://localhost:5000/api/periodes';
 
   constructor(private httpClient: HttpClient) {}
 
   list(classe: Classe) {
     return  this.httpClient.get<Time[]>(this.url);
+  }
+
+  listPeriode(periode:Periode){
+    return this.httpClient.get<Periode[]>(this.urlPeriode);
   }
 }
